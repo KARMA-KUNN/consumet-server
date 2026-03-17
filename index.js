dc3383a343632826be5505495c1d530821dd7c48
KARMA-KUNN <tanishbhagat0777@gmail.com>
2026-03-17 12:29:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,10 @@
- import express from 'express';
+import express from 'express';
 const app = express();
 
-const { ANIME } = await import('@consumet/extensions');
-const gogoanime = new ANIME.Gogoanime();
-
-app.get('/anime/gogoanime/:query', async (req, res) => {
-  try {
-    const data = await gogoanime.search(req.params.query);
-    res.json(data);
-  } catch (e) {
-    res.status(500).json({ error: e.message });
-  }
-});
-
-app.get('/anime/gogoanime/info/:id', async (req, res) => {
-  try {
-    const data = await gogoanime.fetchAnimeInfo(req.params.id);
-    res.json(data);
-  } catch (e) {
-    res.status(500).json({ error: e.message });
-  }
-});
-
-app.get('/anime/gogoanime/watch/:episodeId', async (req, res) => {
-  try {
-    const data = await gogoanime.fetchEpisodeSources(req.params.episodeId);
-    res.json(data);
-  } catch (e) {
-    res.status(500).json({ error: e.message });
-  }
+app.get('/test', async (req, res) => {
+  const mod = await import('@consumet/extensions');
+  const animeKeys = Object.keys(mod.ANIME);
+  res.json({ animeKeys });
 });
 
 app.listen(process.env.PORT || 3000, () => {
