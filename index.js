b8a31bd4ad1bfbcd07a18bb68d265990c1757bc9
KARMA-KUNN <tanishbhagat0777@gmail.com>
2026-03-17 12:36:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-import express from 'express';
+ import express from 'express';
 const app = express();
 
-app.get('/test', async (req, res) => {
-  const mod = await import('@consumet/extensions');
-  const animeKeys = Object.keys(mod.ANIME);
-  res.json({ animeKeys });
+const { ANIME } = await import('@consumet/extensions');
+const hianime = new ANIME.Hianime();
+
+app.get('/anime/hianime/:query', async (req, res) => {
+  try {
+    const data = await hianime.search(req.params.query);
+    res.json(data);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+app.get('/anime/hianime/info/:id', async (req, res) => {
+  try {
+    const data = await hianime.fetchAnimeInfo(req.params.id);
+    res.json(data);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+app.get('/anime/hianime/watch/:episodeId', async (req, res) => {
+  try {
+    const data = await hianime.fetchEpisodeSources(req.params.episodeId);
+    res.json(data);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
 });
 
 app.listen(process.env.PORT || 3000, () => {
