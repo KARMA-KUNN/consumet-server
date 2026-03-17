4a3cd4b510f58f354b4115996a76dd9bb6ecd754
KARMA-KUNN <tanishbhagat0777@gmail.com>
2026-03-17 10:36:00 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+const { ANIME } = require('@consumet/extensions');
+const express = require('express');
+const app = express();
+const gogoanime = new ANIME.Gogoanime();
+
+app.get('/anime/gogoanime/:query', async (req, res) => {
+  try {
+    const data = await gogoanime.search(req.params.query);
+    res.json(data);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+app.get('/anime/gogoanime/info/:id', async (req, res) => {
+  try {
+    const data = await gogoanime.fetchAnimeInfo(req.params.id);
+    res.json(data);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+app.get('/anime/gogoanime/watch/:id', async (req, res) => {
+  try {
+    const data = await gogoanime.fetchEpisodeSources(req.params.id);
+    res.json(data);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+app.listen(process.env.PORT || 3000, () => console.log('Server running'));
